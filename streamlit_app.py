69c05dda97eff8d5c3cf87a1178d213f6c3ac7c3
Bryan Moore <bryanallenmoe@gmail.com>
2023-01-19 10:50:03 -0800

    More inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.title('WEB INVENTORY LOOKUP TOOL')
-st.write('When you’re wondering why a SKU isn’t showing up online, this tool has your back!')
 
 # Sidebar
 with st.sidebar:
     st.title('INPUTS')
+    st.text_input('6-DIGIT SKU')
+    st.selectbox('COUNTRY', ['US', 'CA'])
 
-with st.sidebar:
-    st.text_input('Enter some text')
+st.title('WEB INVENTORY LOOKUP TOOL')
+st.write('When you’re wondering why a SKU isn’t showing up online, this tool has your back!')
 
+
+#Main Section
 
