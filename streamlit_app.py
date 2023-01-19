e722ff1f99cd2b9fd62451fbd47c7768887f3620
Bryan Moore <bryanallenmoe@gmail.com>
2023-01-19 10:54:51 -0800

    More styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,22 @@ with st.sidebar:
     st.title('INPUTS')
     st.text_input('6-DIGIT SKU')
     st.selectbox('COUNTRY', ['US', 'CA'])
+    st.button('RUN IT!')
 
+#Main Section
 st.title('WEB INVENTORY LOOKUP TOOL')
 st.write('When you’re wondering why a SKU isn’t showing up online, this tool has your back!')
 
 col1, col2 = st.columns((1,1))
 
 with col1:
-    st.title('Column 1')
+    st.header('ITEM INFO')
 
 with col2:
-    st.title('Column 2')
+    st.header('TESTS')
+
+
+
 
 
 #Main Section
