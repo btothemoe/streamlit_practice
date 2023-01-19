4947af22899e60b3c2bcdb3d851b415d8fceaa49
Bryan Moore <bryanallenmoe@gmail.com>
2023-01-19 09:44:23 -0800

    Playing around with layouts

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title('My title')
 st.title('Lets see what we can do here!')
+
+# Input widgets in the sidebar
+with st.sidebar:
+	st.title('My title')
